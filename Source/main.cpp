6386c4a936959b872c453ce8585700978788ee37
Spidre <52509987+LouisT123@users.noreply.github.com>
2023-04-17 14:27:32 -0700

    AI1 command

--- Source/main.cpp
+++ Source/main.cpp
@@ -7,8 +7,8 @@ const int BUFSIZE = 140;
 wchar_t COMPORT_Rx[] = L"COM7"; // COM port used for Rx (use L"COM6" for transmit program)
 wchar_t COMPORT_Tx[] = L"COM6";// COM port used for Tx (use L"COM6" for transmit program)
 
-wchar_t COMPORT1 = "COM1"
-wchar_t COMPORT2 = "COM2"
+wchar_t COMPORT1[] = L"COM1";
+wchar_t COMPORT2[] = L"COM2";
 //com needs to have:
 //4800 baudrate
 //no parity
@@ -48,13 +48,42 @@ int main()
 void COM1()
 {
   cout << "Opening COM1 at 4800 baud" << endl;
-  /*initPort(&hSerialRx, COMPORT1, nComRate, nComBits, timeout); //rx port
+  initPort(&hSerialRx, COMPORT1, nComRate, nComBits, timeout); //rx port
   Sleep(500);
-  */
+
   initPort(&hSerialTx, COMPORT1, nComRate, nComBits, timeout); //Tx port
   Sleep(500);
+
+  cout << "Checking heading for the rotator" << endl;
+  //ask for user input for command later, for now check heading
   //need to transmit based on appendix F in rt21 manual (pg 47)
+  char commandOut[] = "AI1";
+  //cin >> COMPT;
+
+  //send string to port
+  outputToPort(&hSerialTx, messageOut, strlen(messageOut)+1);
+  Sleep(500);
+ 
+  // Reciever side 
+  char messageIn[BUFSIZE];
+  DWORD bytesRead;
+  //grab string from port
+  bytesRead = inputFromPort(&hSerialRx, messageIn, BUFSIZE);
+
+  //length of recieved message
+  messageIn[bytesRead] = '\0';
+  // Display message from port
+  printf("\nMessage Received: %s\n\n", msgIn);				
   
+
+  // Tear down both sides of the comm link
+  /*
+	purgePort(&hComRx);											// Purge the Rx port
+	purgePort(&hComTx);											// Purge the Tx port
+	CloseHandle(hComRx);										// Close the handle to Rx port 
+	CloseHandle(hComTx);										// Close the handle to Tx port 
+	*/
+	system("pause");
 }
 
 void COM2()
